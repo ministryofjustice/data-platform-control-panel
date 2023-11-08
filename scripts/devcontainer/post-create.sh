c30d0f928bb8b1b6e6d7683f9036b094490594f9
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-11-08 12:27:20 +0000

    test pre-commit
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- scripts/devcontainer/post-create.sh
+++ scripts/devcontainer/post-create.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Fix directory
-# git config --global --add safe.directory /workspaces/data-platform-control-panel
-
 # Upgrade NPM
 npm install --global npm@latest
 
@@ -16,4 +13,4 @@ pip install --upgrade pip
 pip install -r requirements.dev.txt
 
 # Install precommit hooks
-# pre-commit install
+pre-commit install
