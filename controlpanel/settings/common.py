88d5742ecc42212b0f37f6a2e92d8e8a8fa75479
ymao2 <yikang.mao@digital.justice.gov.uk>
2023-11-09 13:22:45 +0000

    Change the line of  reading value for  secret-key (#7)
    Remove the value of secret-key from commons

--- controlpanel/settings/common.py
+++ controlpanel/settings/common.py
@@ -20,8 +20,7 @@ PROJECT_NAME = "controlpanel"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!-2v1ja--!*$lz3q6ox+(_d_cc68d5s#72ia-*_&!dom3#$zjn"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
