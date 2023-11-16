2a9d3e9d7b8538847f09759fb19b34b65f3fd027
ymao2 <yikang.mao@digital.justice.gov.uk>
2023-11-16 13:18:12 +0000

    isort warning

--- controlpanel/interfaces/web/urls.py
+++ controlpanel/interfaces/web/urls.py
@@ -1,7 +1,6 @@
-from controlpanel.interfaces.web import views
-
 from django.urls import path
 
+from controlpanel.interfaces.web import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
