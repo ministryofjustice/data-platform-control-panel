572542a0fe04cdea1ca2b386b894e9f1ffc52a9b
ymao2 <yikang.mao@digital.justice.gov.uk>
2023-11-17 01:33:02 +0000

    fixed the black issue

--- controlpanel/core/migrations/0001_initial.py
+++ controlpanel/core/migrations/0001_initial.py
@@ -27,7 +27,7 @@ class Migration(migrations.Migration):
                     models.BooleanField(
                         default=False,
                         help_text="Designates that this user has all permissions without "
-                                  "explicitly assigning them.",
+                        "explicitly assigning them.",
                         verbose_name="superuser status",
                     ),
                 ),
@@ -36,7 +36,7 @@ class Migration(migrations.Migration):
                     models.CharField(
                         error_messages={"unique": "A user with that username already exists."},
                         help_text="Required. 150 characters or fewer. Letters, "
-                                  "digits and @/./+/-/_ only.",
+                        "digits and @/./+/-/_ only.",
                         max_length=150,
                         unique=True,
                         validators=[django.contrib.auth.validators.UnicodeUsernameValidator()],
@@ -68,7 +68,7 @@ class Migration(migrations.Migration):
                     models.BooleanField(
                         default=True,
                         help_text="Designates whether this user should be treated as active. "
-                                  "Unselect this instead of deleting accounts.",
+                        "Unselect this instead of deleting accounts.",
                         verbose_name="active",
                     ),
                 ),
@@ -89,7 +89,7 @@ class Migration(migrations.Migration):
                     models.ManyToManyField(
                         blank=True,
                         help_text="The groups this user belongs to. A user will get all "
-                                  "permissions granted to each of their groups.",
+                        "permissions granted to each of their groups.",
                         related_name="user_set",
                         related_query_name="user",
                         to="auth.group",
