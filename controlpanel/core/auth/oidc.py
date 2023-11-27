7554125b45e7a62d12fb1d33a7050f4332d2acb6
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2023-11-27 15:28:21 +0000

    Remove unused imports

--- controlpanel/core/auth/oidc.py
+++ controlpanel/core/auth/oidc.py
@@ -1,10 +1,8 @@
 from django.conf import settings
-from django.dispatch import receiver
-from django.urls import reverse
 from django.utils import timezone
 
 import structlog
-from authlib.integrations.django_client import OAuth, OAuthError, token_update
+from authlib.integrations.django_client import OAuth
 
 from controlpanel.core.models.user import User
 
@@ -18,21 +16,6 @@ oauth.register(
     server_metadata_url=settings.AUTHLIB_OAUTH_CLIENTS["azure"]["server_metadata_url"],
     client_kwargs=settings.AUTHLIB_OAUTH_CLIENTS["azure"]["client_kwargs"],
 )
-
-
-@receiver(token_update)
-def on_token_update(sender, token, refresh_token=None, access_token=None, **kwargs):
-    """for information purpose only
-    Since this app (so far) doesn't use access_token linking to the user directly
-    the refresh_token is not very useful here, even app will use the access_token
-    in the future,  using the auto token_update signal from authlib, the problem will
-    be where we store the tokens (access_token and refresh_token), if we store them in
-    the session, this token_update won't allow app to access current session object
-    from current request, alternatively we can use django-crequest or refresh the
-    access_token manually
-    """
-    if refresh_token:
-        logger.info("The token has been refreshed by using refresh_token")
 
 
 class OIDCSubAuthenticationBackend:
@@ -94,19 +77,6 @@ class OIDCSessionValidator:
     def __init__(self, request):
         """refresh needs to attach the current request"""
         self.request = request
-
-    def _refresh_by_silence_auth(self):
-        """TBD should we re-auth silent before token is expired just for covering
-        the timing issue if someone is removed from IDP, but this user is still valid user
-        and has valid session with Control panel at that moment?
-        """
-        try:
-            redirect_uri = self.request.build_absolute_uri(reverse("authenticate"))
-            # TODO update if/when selent auth is enabled
-            return oauth.azure.authorize_redirect(self.request, redirect_uri, prompt="none")
-        except OAuthError as ex:
-            logger.debug("Failed to perform silence-auth due to error (%s)", ex.__str__())
-            return False
 
     def _has_access_token_expired(self):
         current_seconds = timezone.now().timestamp()
