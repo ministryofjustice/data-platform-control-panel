60258c11c6160a40adf53277976eae375b288358
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2023-12-01 16:16:14 +0000

    Move header and nav context to base view

--- controlpanel/interfaces/web/views.py
+++ controlpanel/interfaces/web/views.py
@@ -1,17 +1,62 @@
-from django.shortcuts import render
-from django.views.generic import View
+from typing import Any
+
+from django.urls import reverse
+from django.views.generic import TemplateView
 
 from controlpanel.core.models import User
 from controlpanel.interfaces.web.auth.mixins import OIDCLoginRequiredMixin
 
 
-class IndexView(OIDCLoginRequiredMixin, View):
+class BaseView(OIDCLoginRequiredMixin, TemplateView):
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        """
+        Adds context used in the nav and header
+        """
+        context = super().get_context_data(**kwargs)
+        context["nav_items"] = self.get_nav_items()
+        context.update(self.get_header_context())
+        return context
+
+    def get_nav_items(self):
+        return [
+            {"name": "Home", "url": "/", "active": self.request.get_full_path() == "/"},
+            {
+                "name": "Data Products",
+                "url": reverse("data-products"),
+                "active": self.request.get_full_path() == reverse("data-products"),
+            },
+        ]
+
+    def get_header_context(self):
+        login_logout_url = (
+            reverse("logout") if self.request.user.is_authenticated else reverse("login")
+        )
+        return {
+            "header_nav_items": [
+                {
+                    "name": self.request.user.name,
+                    "url": "#tbc",
+                    "active": True,
+                },
+                {
+                    "name": "Sign out" if self.request.user.is_authenticated else "Sign in",
+                    "url": login_logout_url,
+                    "active": self.request.get_full_path() == login_logout_url,
+                },
+            ],
+            "header_organisation_url": "https://www.gov.uk/government/organisations/ministry-of-justice",  # noqa
+            "header_service_url": "https://github.com/ministryofjustice/data-platform",
+        }
+
+
+class IndexView(BaseView):
     template_name = "home.html"
 
-    def get(self, request):
-        users = User.objects.all()
-        context = {
-            "user": request.user,
-            "users": users,
-        }
-        return render(request, "home.html", context=context)
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {
+                "users": User.objects.all(),
+            }
+        )
+        return context
--- controlpanel/settings/common.py
+++ controlpanel/settings/common.py
@@ -96,8 +96,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "controlpanel.interfaces.web.context_processors.nav_items",
-                "controlpanel.interfaces.web.context_processors.header_context",
             ],
         },
     },
