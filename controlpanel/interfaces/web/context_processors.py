59bef98403b2cfb54af928e2d82a85281d49388d
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2023-12-05 17:21:44 +0000

    Revert to using context_processors in header
    Decided to use context_processors rather than adding a view mixin, as
    using a mixin means remembering to use it for every view that you add.
    Also reduces the chance that it is overwritten accidentally for a view.
    Using context processors means it is defined once, in one place, and
    will always be used.

--- controlpanel/interfaces/web/context_processors.py
+++ controlpanel/interfaces/web/context_processors.py
@@ -17,18 +17,15 @@ def nav_items(request):
 
 def header_context(request):
     is_logged_in = request.user.is_authenticated
-    login_or_out_url = reverse("logout") if is_logged_in else reverse("login")
     return {
         "header_nav_items": [
             {
                 "name": request.user.name,
-                "url": "#tbc",
-                "active": True,
+                "url": "",
             },
             {
                 "name": "Sign out" if is_logged_in else "Sign in",
-                "url": login_or_out_url,
-                "active": request.get_full_path() == login_or_out_url,
+                "url": reverse("logout") if is_logged_in else reverse("login"),
             },
         ],
         "header_organisation_url": "https://www.gov.uk/government/organisations/ministry-of-justice",  # noqa
--- controlpanel/interfaces/web/views.py
+++ controlpanel/interfaces/web/views.py
@@ -46,7 +46,7 @@ class BaseView(OIDCLoginRequiredMixin, TemplateView):
         }
 
 
-class IndexView(BaseView):
+class IndexView(OIDCLoginRequiredMixin, TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
