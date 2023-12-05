8612bbb24214a31a7a3415d2b05130cff634392d
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2023-12-05 17:26:19 +0000

    Remove BaseView

--- controlpanel/interfaces/web/views.py
+++ controlpanel/interfaces/web/views.py
@@ -1,49 +1,9 @@
 from typing import Any
 
-from django.urls import reverse
 from django.views.generic import TemplateView
 
 from controlpanel.core.models import User
 from controlpanel.interfaces.web.auth.mixins import OIDCLoginRequiredMixin
-
-
-class BaseView(OIDCLoginRequiredMixin, TemplateView):
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        """
-        Adds context used in the nav and header
-        """
-        context = super().get_context_data(**kwargs)
-        context["nav_items"] = self.get_nav_items()
-        context.update(self.get_header_context())
-        return context
-
-    def get_nav_items(self) -> list[dict]:
-        return [
-            {"name": "Home", "url": "/", "active": self.request.get_full_path() == "/"},
-            {
-                "name": "Data Products",
-                "url": reverse("data-products"),
-                "active": self.request.get_full_path() == reverse("data-products"),
-            },
-        ]
-
-    def get_header_context(self) -> dict[str, Any]:
-        return {
-            "header_nav_items": [
-                {
-                    "name": self.request.user.name,
-                    "url": "",
-                },
-                {
-                    "name": "Sign out" if self.request.user.is_authenticated else "Sign in",
-                    "url": reverse("logout")
-                    if self.request.user.is_authenticated
-                    else reverse("login"),
-                },
-            ],
-            "header_organisation_url": "https://www.gov.uk/government/organisations/ministry-of-justice",  # noqa
-            "header_service_url": "https://github.com/ministryofjustice/data-platform",
-        }
 
 
 class IndexView(OIDCLoginRequiredMixin, TemplateView):
